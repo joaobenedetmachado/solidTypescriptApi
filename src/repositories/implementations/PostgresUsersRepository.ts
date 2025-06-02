3fd637abaad343b8b55c1eb1330df1f135738cc5
joaobenedetmachado <jvbm2177@gmail.com>
2025-06-02 11:41:19 +0000

    fix: returning user when found

--- src/repositories/implementations/PostgresUsersRepository.ts
+++ src/repositories/implementations/PostgresUsersRepository.ts
@@ -3,10 +3,17 @@ import { IUsersRepository } from "../IUsersRepository";
 
 export class PostgresUsersRepository implements IUsersRepository {
     private users: User[] = [];
-    
+
     async findByEmail(email: string): Promise<User> {
-        const user = this.users.find(user => user.email === email)
+        const user = this.users.find(user => user.email === email);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        return user;
     }
+
 
     async save(user: User): Promise<void> {
         this.users.push(user)
