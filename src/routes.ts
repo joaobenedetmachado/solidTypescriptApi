4fc92ca25db7e59d1d350463b8970b199e2fd1cc
joaobenedetmachado <jvbm2177@gmail.com>
2025-06-02 11:43:29 +0000

    fix: importing Response, Request

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { createUserController } from "./useCases/createUser";
 
 const router = Router();
 
-router.post('/user', (request, response) => {
-    return response.status(201).send()
+router.post('/user', async (request: Request, response: Response) => {
+    return await createUserController.handle(request, response);
 });
 
-export { router }
+
+export { router };
